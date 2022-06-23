548f7125dc78491b226f1304b83aefb3a58109d2
Bociarz1 <mikrofoncrk@gmail.com>
2022-06-23 17:43:35 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react"
 
 function App() {
+  const [count,setCount] = useState(0)
+  const [randomNum,setRandomNum] = useState(0)
+
+//  2. create increase function
+  const increase = () => {
+    setCount(count+1)
+  }
+  
+  // 5. wrap with useMemo funcions that you want to check if there are changes/ isEven cache VALUE
+  const isEven = useMemo(() => {
+    let i = 0
+    while(i<1000000000) i++
+    if(count % 2 === 0) return "even" 
+    else return "odd"
+  },[count])
+
+  // 4. create other functions
+  const random = () => {
+    setRandomNum(Math.floor(Math.random() * 100))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h4>Counter: {count} <br/> Number is:{isEven}</h4>
+      {/* 1. create increase counter button */}
+      <button onClick={increase}>Increase</button>
+      <h4>Random number:{randomNum}</h4>
+      {/* 3. create other functionality */}
+      <button onClick={random}>Generate random number</button>
+    </>
   );
 }
 
